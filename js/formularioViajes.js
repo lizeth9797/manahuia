6883c20dcab74677c3246351921eb536294e745d
Ditartemisa <dianapfernandez@uned.cr>
2024-03-01 00:02:01 -0600

    Se corrigen errores en el código

--- js/formularioViajes.js
+++ js/formularioViajes.js
@@ -1,272 +1,218 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // obtiene el ultimo contador del input almacenado en localStorage iniciando en 11
     let idCounter = parseInt(localStorage.getItem('idCounter')) || 11;
 
-  let btnSendTravel = document.getElementById('btnSendTravel');
-  let nameTravel = document.getElementById('nameTravel');
-  let nameTravelError = document.getElementById('nameTravelError');
-  let checkboxes = [
-      document.getElementById('btncheck1'),
-      document.getElementById('btncheck2'),
-      document.getElementById('btncheck3'),
-      document.getElementById('btncheck4'),
-      document.getElementById('btncheck5'),
-      document.getElementById('btncheck6'),
-      document.getElementById('btncheck7'),
-  ];
-  let checkboxesError = document.getElementById("checkboxesError");
-  let description = document.getElementById('description');
-  let descriptionError = document.getElementById('descriptionError');
-  let priceInput = document.getElementById('priceInput');
-  let priceInputError = document.getElementById('priceInputError');
-  let uploadPhotos = document.getElementById('uploadPhotos');
-  let uploadPhotosError = document.getElementById('uploadPhotosError');
-  let startDateInput = document.getElementById('startDate');
-  let finalDateInput = document.getElementById('finalDate');
-  let startDateError = document.getElementById('startDateError');
-  let finalDateError = document.getElementById('finalDateError');
-  const cloudName = "dezqwhec1"; 
-  const uploadPreset = "ip5jqq1p"; 
+    let btnSendTravel = document.getElementById('btnSendTravel');
+    let nameTravel = document.getElementById('nameTravel');
+    let nameTravelError = document.getElementById('nameTravelError');
+    let checkboxes = [
+        document.getElementById('btncheck1'),
+        document.getElementById('btncheck2'),
+        document.getElementById('btncheck3'),
+        document.getElementById('btncheck4'),
+        document.getElementById('btncheck5'),
+        document.getElementById('btncheck6'),
+        document.getElementById('btncheck7'),
+    ];
+    let checkboxesError = document.getElementById("checkboxesError");
+    let description = document.getElementById('description');
+    let descriptionError = document.getElementById('descriptionError');
+    let priceInput = document.getElementById('priceInput');
+    let priceInputError = document.getElementById('priceInputError');
+    let uploadPhotos = document.getElementById('uploadPhotos');
+    let uploadPhotosError = document.getElementById('uploadPhotosError');
+    let startDateInput = document.getElementById('startDate');
+    let finalDateInput = document.getElementById('finalDate');
+    let startDateError = document.getElementById('startDateError');
+    let finalDateError = document.getElementById('finalDateError');
+    const cloudName = "dezqwhec1"; 
+    const uploadPreset = "ip5jqq1p"; 
 
-    let imagenesSubidas=[]
-    //pinta en el HTML la ventana para subir una imágen
+    let imagenesSubidas = [];
+
     const myWidget = cloudinary.createUploadWidget(
         {
-        cloudName: cloudName,
-        uploadPreset: uploadPreset,
+            cloudName: cloudName,
+            uploadPreset: uploadPreset,
         },
         (error, result) => {
-        if (!error && result && result.event === "success") {
-            //console.log("Done! Here is the image info: ", result);
-            imagenesSubidas.push(result.info.secure_url);
-            //console.log("imagenesSubidasARRAY:",imagenesSubidas);
-        }
+            if (!error && result && result.event === "success") {
+                imagenesSubidas.push(result.info.secure_url);
+            }
         }
     );
-    document.getElementById("upload_widget").addEventListener("click",function () {
+
+    document.getElementById("upload_widget").addEventListener("click", function () {
         myWidget.open();
-        },  false
-    );
+    }, false);
 
-
-
-
-  function calcularDuracion(startDateInput, finalDateInput) {
-    const start = new Date(startDateInput);
-    const final = new Date(finalDateInput);
-    const diferenciaEnTiempo = final - start;
-    const diferenciaEnDias = Math.ceil(diferenciaEnTiempo / (1000 * 60 * 60 * 24));
-    return diferenciaEnDias;
-}
-
-function addItem(nuevoViaje) {
-    console.log("Nuevo Viaje:", nuevoViaje);
-    let viajes = JSON.parse(localStorage.getItem('viajes')) || [];
-
-        // esto lo que hace es agarrar el objeto del nuevo viaje al array de viajes
-        viajes.push(nuevoViaje);
-
-        // almacenar el array de objetos y actualizarlo en localStorage
-        localStorage.setItem('viajes', JSON.stringify(viajes));
-}
-
-//BOTON CREAR
-  btnSendTravel.addEventListener('click', function (event) {
-      event.preventDefault();
-      let valid = true;
-
-      if (!validarFechas()) {
-        valid = false;
+    function calcularDuracion(startDateInput, finalDateInput) {
+        const start = new Date(startDateInput);
+        const final = new Date(finalDateInput);
+        const diferenciaEnTiempo = final - start;
+        const diferenciaEnDias = Math.ceil(diferenciaEnTiempo / (1000 * 60 * 60 * 24));
+        return diferenciaEnDias;
     }
 
-        //Obtener y procesar las imágenes en JSON
-/*         const inputFiles = document.getElementById('uploadPhotos');
-        const files = inputFiles.files;
-        const imagenesSubidas = [];
-        for (let i = 0; i < files.length; i++) {
-            let nuevaImagen = files[i];
-            imagenesSubidas.push({ nombre: nuevaImagen.name, tipo: nuevaImagen.type, tamaño: nuevaImagen.size });
-        } */
+    function addItem(nuevoViaje) {
+        let viajes = JSON.parse(localStorage.getItem('viajes')) || [];
+        viajes.push(nuevoViaje);
+        localStorage.setItem('viajes', JSON.stringify(viajes));
+    }
 
-      // Validación del nombre del destino
-      if (nameTravel.value.length < 10 || /^\s|\s$/.test(nameTravel.value) || /\s{2,}/.test(nameTravel.value)) {
-          nameTravel.classList.add('is-invalid');
-          nameTravelError.textContent = 'Por favor escribe un nombre de viaje válido (mínimo 10 letras)';
-          valid = false;
-      } else {
-          nameTravel.classList.remove('is-invalid');
-          nameTravel.classList.add('is-valid');
-          nameTravelError.textContent = ''; // Limpiar el mensaje de error
-      }
+    btnSendTravel.addEventListener('click', function (event) {
+        event.preventDefault();
+        let valid = true;
 
-      // Validación de fechas
-      validarFechas();
+        if (!validarFechas()) {
+            valid = false;
+        }
 
-      // Validación de checkboxes
+        if (nameTravel.value.length < 10 || /^\s|\s$/.test(nameTravel.value) || /\s{2,}/.test(nameTravel.value)) {
+            nameTravel.classList.add('is-invalid');
+            nameTravelError.textContent = 'Por favor escribe un nombre de viaje válido (mínimo 10 letras)';
+            valid = false;
+        } else {
+            nameTravel.classList.remove('is-invalid');
+            nameTravel.classList.add('is-valid');
+            nameTravelError.textContent = '';
+        }
+
+        validarFechas();
+
         let selectedCheckboxes = checkboxes.filter(checkbox => checkbox.checked);
-        
+
         if (selectedCheckboxes.length < 2) {
             checkboxesError.textContent = 'Por favor escoge mínimo 2 opciones';
             valid = false;
         } else {
-            checkboxesError.textContent = ''; // Limpiar el mensaje de error
+            checkboxesError.textContent = '';
         }
 
-        // Obtener el texto de los labels asociados a los checkboxes seleccionados
         const incluyeText = selectedCheckboxes.map(checkbox => {
             const label = document.querySelector(`label[for=${checkbox.id}]`);
             return label.textContent.trim();
         }).join(', ');
 
-      // Validación de la descripción
-      if (description.value.length === 0 || description.value.length > 300) {
-          description.classList.add('is-invalid');
-          descriptionError.textContent = 'Por favor escribe un comentario menor a 300 caracteres';
-          valid = false;
-      } else {
-          description.classList.remove('is-invalid');
-          description.classList.add('is-valid');
-          descriptionError.textContent = ''; // Limpiar el mensaje de error
-      }
+        if (description.value.length === 0 || description.value.length > 300) {
+            description.classList.add('is-invalid');
+            descriptionError.textContent = 'Por favor escribe un comentario menor a 300 caracteres';
+            valid = false;
+        } else {
+            description.classList.remove('is-invalid');
+            description.classList.add('is-valid');
+            descriptionError.textContent = '';
+        }
 
-      // Validación del precio
-      let priceInputValue = priceInput.value.trim();
+        let priceInputValue = priceInput.value.trim();
 
-      // Validar que el campo de precio solo contenga números
-      if (!/^\d+$/.test(priceInputValue)) {
-          priceInput.classList.add('is-invalid');
-          priceInputError.textContent = 'Ingresa solo números en el campo de precio.';
-          valid = false;
-          
-      } else {
-          let priceValue = parseFloat(priceInputValue.replace(',', ''));
-          priceInput.classList.remove('is-invalid');
-          priceInput.classList.add('is-valid');
-          priceInputError.textContent = '';
-      
-          if (isNaN(priceValue) || priceValue <= 0 || priceValue >= 10000000) {
-              priceInput.classList.add('is-invalid');
-              priceInputError.textContent = 'Agrega una cifra válida';
-              valid = false;
-              
-          } else {
-              priceInput.classList.remove('is-invalid');
-              priceInput.classList.add('is-valid');
-              priceInputError.textContent = '';
-          }
-      }
+        if (!/^\d+$/.test(priceInputValue)) {
+            priceInput.classList.add('is-invalid');
+            priceInputError.textContent = 'Ingresa solo números en el campo de precio.';
+            valid = false;
+        } else {
+            let priceValue = parseFloat(priceInputValue.replace(',', ''));
+            priceInput.classList.remove('is-invalid');
+            priceInput.classList.add('is-valid');
+            priceInputError.textContent = '';
 
-      // Validación de la carga de fotos
-      if((imagenesSubidas.length!=4)){   
-        console.log(imagenesSubidas.length);
-        //uploadPhotos.classList.add('is-invalid');
-        uploadPhotosError.textContent = 'Por favor sube 4 fotografías\n';
-        valid = false;
-      }else {
-       // uploadPhotos.classList.remove('is-invalid');
-        //uploadPhotos.classList.add('is-valid');
-        uploadPhotosError.textContent = ''; // Limpiar el mensaje de error
+            if (isNaN(priceValue) || priceValue <= 0 || priceValue >= 10000000) {
+                priceInput.classList.add('is-invalid');
+                priceInputError.textContent = 'Agrega una cifra válida';
+                valid = false;
+            } else {
+                priceInput.classList.remove('is-invalid');
+                priceInput.classList.add('is-valid');
+                priceInputError.textContent = '';
+            }
+        }
+
+        if (imagenesSubidas.length !== 4) {
+            uploadPhotosError.textContent = 'Por favor sube 4 fotografías\n';
+            valid = false;
+        } else {
+            uploadPhotosError.textContent = '';
+        }
+
+        if (valid) {
+            var nuevoViaje = {
+                'id': idCounter,
+                'nombreDestino': nameTravel.value,
+                'precio': priceInput.value,
+                'incluye': incluyeText,
+                'fechaInicio': startDate.value,
+                'fechaFin': finalDate.value,
+                'duracion': calcularDuracion(startDateInput.value, finalDateInput.value),
+                'descripcion': description.value,
+                'img': imagenesSubidas
+            };
+
+            addItem(nuevoViaje);
+            $('#successModal').modal('show');
+            idCounter++;
+            localStorage.setItem('idCounter', idCounter.toString());
+        } else {
+            $('#errorModal').modal('show');
+        }
+    });
+
+    function validarFechas() {
+        const fechaActual = new Date();
+        const startDate = new Date(startDateInput.value);
+        const finalDate = new Date(finalDateInput.value);
+        const hoy = fechaActual.toISOString().split('T')[0];
+        const fechaMaxima = new Date(fechaActual);
+        fechaMaxima.setFullYear(fechaMaxima.getFullYear() + 1);
+
+        startDateError.textContent = '';
+        finalDateError.textContent = '';
+
+        if (startDate < fechaActual) {
+            startDateError.textContent = 'La fecha de inicio no puede ser menor al día actual.';
+            startDateInput.value = hoy;
+            return false;
+        }
+
+        if (!startDateInput.value) {
+            startDateError.textContent = 'Por favor ingresa una fecha de inicio.';
+            return false;
+        } else if (startDate < fechaActual) {
+            startDateError.textContent = 'La fecha de inicio debe ser mayor al día actual y diferente a la fecha final';
+            startDateInput.value = hoy;
+            return false;
+        }
+
+        if (finalDate < startDate) {
+            finalDateError.textContent = 'La fecha final debe ser mayor al día actual y diferente a la fecha final.';
+            finalDateInput.value = startDateInput.value;
+            return false;
+        }
+        
+        if (!finalDateInput.value) {
+            finalDateError.textContent = 'Por favor ingresa una fecha final.';
+            return false;
+        } else if (finalDate < startDate) {
+            finalDateError.textContent = 'La fecha final no puede ser menor o igual a la fecha inicial.';
+            finalDateInput.value = startDateInput.value;
+            return false;
+        }
+
+        if (startDate > fechaMaxima || finalDate > fechaMaxima) {
+            startDateError.textContent = 'Las fechas no pueden superar un año desde hoy.';
+            startDateInput.value = hoy;
+            finalDateInput.value = hoy;
+            finalDateError.textContent = 'Las fechas no pueden superar un año desde hoy.';
+            return false;
+        }
+
+        actualizarClases(startDateInput, startDateError);
+        actualizarClases(finalDateInput, finalDateError);
+
+        return true;
     }
+});
 
-
-// Función para validar fechas
-function validarFechas() {
-    // Obtener la fecha actual
-    const fechaActual = new Date();
-
-    // Convertir los valores de las fechas a objetos Date
-    const startDate = new Date(startDateInput.value);
-    const finalDate = new Date(finalDateInput.value);
-
-    // Obtener la fecha actual en formato YYYY-MM-DD
-    const hoy = fechaActual.toISOString().split('T')[0];
-
-    // Calcular la fecha dentro de un año desde hoy
-    const fechaMaxima = new Date(fechaActual);
-    fechaMaxima.setFullYear(fechaMaxima.getFullYear() + 1);
-
-    // Limpiar los mensajes de error
-    startDateError.textContent = '';
-    finalDateError.textContent = '';
-
-    // Validar la fecha de inicio
-    if (startDate < fechaActual) {
-        startDateError.textContent = 'La fecha de inicio no puede ser menor al día actual.';
-        startDateInput.value = hoy;
-        return false;
-    }
-
-    if (!startDateInput.value) {
-        // Fecha vacía, actualiza el error
-        startDateError.textContent = 'Por favor ingresa una fecha de inicio.';
-        return false;
-    } else if (startDate < fechaActual) {
-        // Fecha de inicio menor al día actual, mostrar alerta y actualizar la fecha
-        startDateError.textContent = 'La fecha de inicio debe ser mayor al día actual y diferente a la fecha final';
-        startDateInput.value = hoy;
-        return false;
-    }
-
-    // Validar la fecha final
-    if (finalDate < startDate) {
-        finalDateError.textContent = 'La fecha final debe ser mayor al día actual y diferente a la fecha final.';
-        finalDateInput.value = startDateInput.value;
-        return false;
-    }
-    if (!finalDateInput.value) {
-        // Fecha vacía, actualiza el error
-        finalDateError.textContent = 'Por favor ingresa una fecha final.';
-        return false;
-    } else if (finalDate < startDate) {
-        // Fecha final menor a la fecha de inicio, mostrar alerta y actualizar la fecha
-        finalDateError.textContent = 'La fecha final no puede ser menor o igual a la fecha inicial.';
-        finalDateInput.value = startDateInput.value;
-        return false;
-    }
-
-    // Validar que las fechas no superen un año
-    if (startDate > fechaMaxima || finalDate > fechaMaxima) {
-        startDateError.textContent = 'Las fechas no pueden superar un año desde hoy.';
-        startDateInput.value = hoy;
-        finalDateInput.value = hoy;
-        finalDateError.textContent = 'Las fechas no pueden superar un año desde hoy.';
-        return false;
-    }
-
-    // Actualizar clases is-valid e is-invalid para fechas
-    actualizarClases(startDateInput, startDateError);
-    actualizarClases(finalDateInput, finalDateError);
-
-    // Si todas las validaciones de fechas son correctas, retorna true
-    return true;
+function actualizarClases(inputElement, errorElement) {
+    inputElement.classList.remove('is-invalid');
+    inputElement.classList.add('is-valid');
+    errorElement.textContent = '';
 }
-    
-
-      // Si todas las validaciones son correctas, se podría enviar el formulario.
-      if (valid) {
-        // Crear el objeto JSON del nuevo viaje
-        var nuevoViaje = {
-            'id': idCounter, // Asignar un nuevo ID
-            'nombreDestino': nameTravel.value,
-            'precio': priceInput.value,
-            'incluye': incluyeText,
-            'fechaInicio': startDate.value,
-            'fechaFin': finalDate.value,
-            'duracion': calcularDuracion(startDateInput.value, finalDateInput.value),
-            'descripcion': description.value,
-            'img': imagenesSubidas
-        };
-    
-        // Llamar a la función addItem con el nuevoViaje como argumento
-        addItem(nuevoViaje);
-        $('#successModal').modal('show');
-    
-        // Incrementar el contador de modales
-        idCounter++;
-        localStorage.setItem('idCounter', idCounter.toString());
-      } else {
-        $('#errorModal').modal('show');
-      }
-      });// btn eventListener
-      
-});// DOMcontent
