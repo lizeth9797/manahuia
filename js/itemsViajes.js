a3d68381a29d91eb675287643b97fdf527e902c3
lizeth9797 <karinaavao@gmail.com>
2024-03-05 14:10:33 -0600

    se actualizan imagenes de viajes por jpg

--- js/itemsViajes.js
+++ js/itemsViajes.js
@@ -127,7 +127,7 @@ addItem({
     'incluye':'Alojamiento en hoteles administrados por manahuia con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte durante todo el recorrido.Visitas guiadas a los principales destinos arqueológicos de la región.<br>Entradas a los sitios arqueológicos mencionados',
     'duracion':'5 días',
     'descripcion':'Embárcate en un apasionante tour de cinco días explorando la rica historia y cultura de la civilización maya en la Península Yucateca. Desde la animada Mérida hasta las impresionantes ruinas de Chichén Itzá, las playas relajantes de Playa del Carmen y las antiguas ruinas de Tulum, nuestro viaje incluye alojamiento de calidad, transporte cómodo y guías expertos para una experiencia inolvidable. Reserva ahora para sumergirte en esta emocionante aventura!',
-    'img':['./src/catalogo/Viaje 5.png','./src/catalogo/Viaje 1.png','./src/catalogo/Viaje 6.png','./src/catalogo/Viaje 32.png']
+    'img':['./src/catalogo/Viaje 5.jpg','./src/catalogo/Viaje 1.jpg','./src/catalogo/Viaje 6.jpg','./src/catalogo/Viaje 32.jpg']
 });
 
 addItem({
@@ -138,7 +138,7 @@ addItem({
     'incluye':'Alojamiento en hoteles seleccionados con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte durante todo el itinerario.<br>Guía turístico experto.',
     'duracion':'4 días',
     'descripcion':'Embárcate en nuestra "Exploración Natural en la Sierra Madre", un viaje de 4 días que te llevará a descubrir la belleza natural y cultural de Puerto Vallarta, San Sebastián del Oeste y Mascota, el paquete incluye alojamiento en hoteles con desayuno y cena, transporte cómodo y un guía turístico experto para una experiencia inolvidable. Únete a nosotros y sumérgete en la magia de la Sierra Madre!',
-        'img':['./src/catalogo/Viaje 3.png','./src/catalogo/Viaje 26.png','./src/catalogo/Viaje 11.png','./src/catalogo/Viaje 16.png']
+        'img':['./src/catalogo/Viaje 3.jpg','./src/catalogo/Viaje 26.jpg','./src/catalogo/Viaje 11.jpg','./src/catalogo/Viaje 16.jpg']
 });
 
 addItem({
@@ -149,7 +149,7 @@ addItem({
     'incluye':'Alojamiento en hoteles seleccionados con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte durante todo el recorrido.<br>Guía turístico experto.',
     'duracion':'6 días',
     'descripcion':'Explora las impresionantes playas de Baja California Sur en nuestro tour de 6 días. Desde las encantadoras costas de Loreto hasta las vibrantes playas de San José del Cabo y La Paz, incluye alojamiento en hoteles con desayuno y cena, transporte cómodo y un guía turístico para asegurar una experiencia inolvidable. Reserva ahora y sumérgete en el paraíso de Baja California Sur!',
-        'img':['./src/catalogo/Viaje 19.png','./src/catalogo/Viaje 21.png','./src/catalogo/Viaje 6.png','./src/catalogo/Viaje 20.png']
+        'img':['./src/catalogo/Viaje 19.jpg','./src/catalogo/Viaje 21.jpg','./src/catalogo/Viaje 6.jpg','./src/catalogo/Viaje 20.jpg']
 });
 
 addItem({
@@ -160,7 +160,7 @@ addItem({
     'incluye':'Alojamiento en hoteles con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte durante todo el recorrido.<br>Visitas guiadas a fincas de café para aprender sobre el proceso de producción.<br>Entradas a parques naturales para explorar la exuberante biodiversidad de Chiapas.',
     'duracion':'7 días',
     'descripcion':'Descubre la magia de Chiapas en nuestra Ruta del Café y Naturaleza. Este paquete de 7 días te sumerge en la cultura del café, con visitas a fincas para conocer su proceso de producción, y te lleva a explorar la naturaleza de la región con entradas a parques naturales. Incluye alojamiento en hoteles con desayuno y cena, transporte y una experiencia inolvidable en Chiapas. Reserva ahora y déjate cautivar por la belleza y el sabor de esta región!',
-        'img':['./src/catalogo/Viaje 18.png','./src/catalogo/Viaje 3.png','./src/catalogo/Viaje 7.png','./src/catalogo/Viaje 22.png']
+        'img':['./src/catalogo/Viaje 18.jpg','./src/catalogo/Viaje 3.jpg','./src/catalogo/Viaje 7.jpg','./src/catalogo/Viaje 22.jpg']
 });
 
 addItem({
@@ -171,7 +171,7 @@ addItem({
     'incluye':'Alojamiento en hoteles con desayuno incluido (8:00am a 10:00am).<br>Transporte durante todo el recorrido.<br>Diversas actividades acuáticas.<br>Guía turístico experto.',
     'duracion':'4 días',
     'descripcion':'Embárcate en nuestra Aventura Marina en la Costa Maya , durante 4 días, explorarás Mahahual,Bacalar y Tulum, disfrutando de actividades acuáticas emocionantes, alojamiento en hoteles condesayuno incluido, transporte cómodo y la guía de un experto turístico. Una experiencia inolvidable enlas cristalinas aguas de la Costa Maya te espera!',
-        'img':['./src/catalogo/Viaje 23.png','./src/catalogo/Viaje 21.png','./src/catalogo/Viaje 24.png','./src/catalogo/Viaje 25.png']
+        'img':['./src/catalogo/Viaje 23.jpg','./src/catalogo/Viaje 21.jpg','./src/catalogo/Viaje 24.jpg','./src/catalogo/Viaje 25.jpg']
 });
 
 addItem({
@@ -182,7 +182,7 @@ addItem({
     'incluye':'Equipo completo de camping.<br>Desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte ida y vuelta desde el punto de encuentro.<br>Guía especializada durante todo el recorrido.',
     'duracion':'3 días',
     'descripcion':'Sumérgete en la naturaleza salvaje de la Reserva de la Biosfera de Sonora con nuestro Eco-Camping,durante 3 días, disfruta de una experiencia única que incluye equipo de camping completo, todas lascomidas, transporte y la guía de un experto en la Reserva de la Biosfera El Pinacate y Gran Desierto deAltar. Una aventura ecológica que no olvidarás!',
-        'img':['./src/catalogo/Viaje 15.png','./src/catalogo/Viaje 14.png','./src/catalogo/Viaje 30.png','./src/catalogo/Viaje 33.png']
+        'img':['./src/catalogo/Viaje 15.jpg','./src/catalogo/Viaje 14.jpg','./src/catalogo/Viaje 30.jpg','./src/catalogo/Viaje 33.jpg']
 });
 
 addItem({
@@ -193,7 +193,7 @@ addItem({
     'incluye':'Alojamiento en hoteles con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte durante todo el recorrido.<br>Guía turística experta.',
     'duracion':'5 días',
     'descripcion':'Embárcate en nuestra emocionante aventura "Encanto y Aventura Volcánica", durante 5 días,explorarás la vibrante Ciudad de México, la histórica Puebla, la fascinante Cholula y el impresionanteParque Nacional Iztaccíhuatl-Popocatépetl. Incluye alojamiento en hoteles con desayuno y cena,transporte cómodo y la guía de una experta turística. Una experiencia única llena de encanto yadrenalina te espera en este viaje volcánico!',
-        'img':['./src/catalogo/Viaje 2.png','./src/catalogo/Viaje 4.png','./src/catalogo/Viaje 10.png','./src/catalogo/Viaje 39.png']
+        'img':['./src/catalogo/Viaje 2.jpg','./src/catalogo/Viaje 4.jpg','./src/catalogo/Viaje 10.jpg','./src/catalogo/Viaje 39.jpg']
 });
 
 addItem({
@@ -204,7 +204,7 @@ addItem({
     'incluye':'Alojamiento en hoteles con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Paseo en globo aerostático sobre Teotihuacán.<br>Entrada a balnearios de aguas termales en Campos Florales y Ixtapan de la Sal.<br>Transporte ida y vuelta desde el punto de encuentro.<br>Guía turística experta.',
     'duracion':'2 días',
     'descripcion':'Vive una experiencia única con nuestra Aventura Aérea y Relajación Natural en México, durante 2 días,disfrutarás de un emocionante paseo en globo aerostático sobre las antiguas pirámides de Teotihuacán,seguido de relajación en los balnearios de aguas termales en Campos Florales e Ixtapan de la Sal. Incluyealojamiento en hoteles con desayuno y cena, transporte ida y vuelta y la guía de una experta turística.Reserva ahora y déjate llevar por esta inolvidable aventura!',
-        'img':['./src/catalogo/Viaje 28.png','./src/catalogo/Viaje 34.png','./src/catalogo/Viaje 36.png','./src/catalogo/Viaje 35.png']
+        'img':['./src/catalogo/Viaje 28.jpg','./src/catalogo/Viaje 34.jpg','./src/catalogo/Viaje 36.jpg','./src/catalogo/Viaje 35.jpg']
 });
 
 addItem({
@@ -215,7 +215,7 @@ addItem({
     'incluye':'Equipo completo de camping.<br>Desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm), durante la estadía<br>Guía experto en montañismo.<br>Transporte ida y vuelta desde el punto de encuentro.',
     'duracion':'4 días',
     'descripcion':'Embárcate en nuestra aventura "Aventura en las Alturas: Montañismo y Camping en México”, durante 4 días, explorarás el majestuoso Parque Nacional Nevado de Toluca, el impresionante Parque NacionalCumbres de Monterrey y el desafiante Parque Nacional Pico de Orizaba. Incluye equipo completo decamping, deliciosos desayunos y cenas, la guía de un experto en montañismo y transporte ida y vuelta.Una experiencia de montaña que nunca olvidarás!',
-        'img':['./src/catalogo/Viaje 38.png','./src/catalogo/Viaje 13.png','./src/catalogo/Viaje 17.png','./src/catalogo/Viaje 12.png']
+        'img':['./src/catalogo/Viaje 38.jpg','./src/catalogo/Viaje 13.jpg','./src/catalogo/Viaje 17.jpg','./src/catalogo/Viaje 12.jpg']
 });
 
 addItem({
@@ -226,5 +226,5 @@ addItem({
     'incluye':'Alojamiento en hoteles con desayuno (8:00am a 10:00am) y cena incluidos (7:00pm a 9:00pm).<br>Transporte hacia destinos turísticos y mercados artesanales.<br>Excursiones a lugares locales de artesanías.<br>Visita a playas paradisíacas.',
     'duracion':'5 días',
     'descripcion':'Embárcate en nuestra experiencia "Encanto Costero y Artesanías Mexicanas”, durante 5 días, disfrutade alojamiento en hoteles con desayuno y cena incluidos, transporte hacia destinos turísticos ymercados artesanales donde podrás explorar y adquirir artesanías únicas, excursiones a lugareslocales de artesanías para conocer de cerca la cultura mexicana y visita a playas paradisíacas pararelajarte y disfrutar del paisaje costero. Una experiencia encantadora que combina lo mejor de la costamexicana y su rica artesanía!',
-        'img':['./src/catalogo/Viaje 27.png','./src/catalogo/Viaje 8.png','./src/catalogo/Viaje 9.png','./src/catalogo/Viaje 31.png']
+        'img':['./src/catalogo/Viaje 27.jpg','./src/catalogo/Viaje 8.jpg','./src/catalogo/Viaje 9.jpg','./src/catalogo/Viaje 31.jpg']
 });
