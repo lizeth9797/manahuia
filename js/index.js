d2fae12f44e576c9d748447845a4b2b7495abf92
jost <jospmdz@gmail.com>
2024-03-05 23:48:12 -0600

    el carrousel del index no funciona correctamente

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 let idCounter = 1;
 
+let cardGroups = [];
+let currentGroupIndex = 0;
+const cardsPerGroup = 2;
+
 function addItem(item) {
     const container = document.getElementById('cards-container');
 
@@ -12,123 +16,113 @@ function addItem(item) {
     idCounter++;
 
     const cardHTML = `
-        <!-- Contenido de la tarjeta -->
-        <img src="${item.img[0]}" class="card-img-top" alt="${item.nombreDestino}">
-        <div class="card-body d-flex flex-column" style="margin: 5px; padding: 5px;">
-            <h5 class="card-title">${item.nombreDestino}</h5>
-            <p class="card-text flex-grow-1">${truncatedDescription}...</p>
-            <button type="button" style="background-color: #85586F" class="btn btn-secondary details-btn" data-bs-toggle="modal" data-bs-target="#${modalId}">
-                Detalles
-            </button>
-        </div>
+    <!-- Contenido de la tarjeta -->
+    <img src="${item.img[0]}" class="card-img-top" alt="${item.nombreDestino}">
+    <div class="card-body d-flex flex-column" style="margin: 5px; padding: 5px;">
+        <h5 class="card-title">${item.nombreDestino}</h5>
+        <p class="card-text flex-grow-1">${truncatedDescription}...</p>
+        <button type="button" style="background-color: #85586F" class="btn btn-secondary details-btn" data-bs-toggle="modal" data-bs-target="#${modalId}">
+            Detalles
+        </button>
+    </div>
 
-        <!-- Modal -->
-        <div class="modal fade custom-modal"  id="exampleModal_${item.id}" tabindex="-1" 
-            aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                    <div class="modal-body row" style= "background-color:#F8EDE3">
-                        <!-- Carrusel de imágenes en el lado izquierdo -->
-                        <div class="col-lg-6" style="align-self: center;">
-                            <div id="${carouselId}" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <!-- Aquí deberías agregar tus imágenes dinámicamente -->
-                                    <div class="carousel-item active">
-                                        <img src="${item.img[0]}" class="d-block w-100" alt="Slide 1">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="${item.img[1]}" class="d-block w-100" alt="Slide 1">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="${item.img[2]}" class="d-block w-100" alt="Slide 1">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="${item.img[3]}" class="d-block w-100" alt="Slide 1">
-                                    </div>
+    <!-- Modal -->
+    <div class="modal fade custom-modal"  id="exampleModal_${item.id}" tabindex="-1" 
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-body row" style= "background-color:#F8EDE3">
+                    <!-- Carrusel de imágenes en el lado izquierdo -->
+                    <div class="col-lg-6" style="align-self: center;">
+                        <div id="${carouselId}" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <!-- Aquí deberías agregar tus imágenes dinámicamente -->
+                                <div class="carousel-item active">
+                                    <img src="${item.img[0]}" class="d-block w-100" alt="Slide 1">
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#${carouselId}" data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button class="carousel-control-next" type="button"   data-bs-target="#${carouselId}" data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
-                                </button>
+                                <div class="carousel-item">
+                                    <img src="${item.img[1]}" class="d-block w-100" alt="Slide 1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="${item.img[2]}" class="d-block w-100" alt="Slide 1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="${item.img[3]}" class="d-block w-100" alt="Slide 1">
+                                </div>
                             </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#${carouselId}" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button"   data-bs-target="#${carouselId}" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
                         </div>
-        
-                        <!-- Informacion a la derecha -->
-                        <div class="col-lg-6">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">${item.nombreDestino}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <p class="modal-description" style="text-align: justify;">${item.descripcion}</p>
-                                <hr />
-                                <h5>Incluye:</h5>
-                                <p class="modal-incluye text-left small">
-                                    ${item.incluye}
-                                </p>
-                                <p class="text-end modal-price"><strong>${item.precio} MXN</strong></p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" style= "background-color:#85586F" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                            </div>
+                    </div>
+    
+                    <!-- Informacion a la derecha -->
+                    <div class="col-lg-6">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">${item.nombreDestino}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="modal-description" style="text-align: justify;">${item.descripcion}</p>
+                            <hr />
+                            <h5>Incluye:</h5>
+                            <p class="modal-incluye text-left small">
+                                ${item.incluye}
+                            </p>
+                            <p class="text-end modal-price"><strong>${item.precio} MXN</strong></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" style= "background-color:#85586F" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div> <!-- cierre-modal -->
-    `;
+        </div>
+    </div> <!-- cierre-modal -->
+`;
 
-    card.innerHTML = cardHTML;
+card.innerHTML = cardHTML;
     
-    // Crear el espacio entre tarjetas
-    const space = document.createElement('div');
-    space.classList.add('col-md-auto'); 
-    
-    // Agregar la tarjeta y el espacio al contenedor
-    container.appendChild(card);
-    container.appendChild(space);
+// Crear el espacio entre tarjetas
+const space = document.createElement('div');
+space.classList.add('col-md-auto'); 
+
+// Agregar la tarjeta y el espacio al contenedor
+container.appendChild(card);
+container.appendChild(space);
+
+// Agregar la tarjeta al grupo actual
+const currentGroup = cardGroups[currentGroupIndex];
+currentGroup.push(card);
+
+// Cargar tarjetas iniciales en el carrusel
+loadCardsIntoCarousel();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    const storedViajes = localStorage.getItem('viajes');
-    const viajes = storedViajes ? JSON.parse(storedViajes) : [];
+const storedViajes = localStorage.getItem('viajes');
+const viajes = storedViajes ? JSON.parse(storedViajes) : [];
 
-    for (let i = 0; i < viajes.length; i++) {
-        const storedItem = viajes[i];
-        addItem(storedItem);
-    }
+// Dividir las tarjetas en grupos
+for (let i = 0; i < viajes.length; i += cardsPerGroup) {
+    const group = [];
+    cardGroups.push(group);
+}
 
-    // Inicializa el carrusel después de agregar las tarjetas
-    const myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleControls'), {
-        interval: 5000,
-        wrap: false
-    });
+// Agrega un evento de clic al contenedor de las tarjetas para manejar clics en los botones
+document.getElementById('cards-container').addEventListener('click', function (event) {
+    if (event.target.classList.contains('details-btn')) {
+        const modalId = event.target.getAttribute('data-bs-target');
+        const modal = new bootstrap.Modal(document.querySelector(modalId));
 
-    // Agrega un evento de clic al contenedor de las tarjetas para manejar clics en los botones del carrusel
-    document.getElementById('carouselExampleControls').addEventListener('click', function (event) {
-        const target = event.target;
-        const isPrevButton = target.getAttribute('data-slide') === 'prev';
-        const isNextButton = target.getAttribute('data-slide') === 'next';
-
-        if (isPrevButton || isNextButton) {
-            myCarousel.isSliding = false;
-            myCarousel[isPrevButton ? 'prev' : 'next']();
-        }
-    });
-
-    // Agrega un evento de clic al contenedor de las tarjetas para manejar clics en los botones
-    document.getElementById('cards-container').addEventListener('click', function (event) {
-        if (event.target.classList.contains('details-btn')) {
-            const modalId = event.target.getAttribute('data-bs-target');
-            const modal = new bootstrap.Modal(document.querySelector(modalId));
-
-            modal.show();
-        }
-    });
+        modal.show();
+     }
+ });
 });
 
 
