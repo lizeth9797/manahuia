3a6fa326e1534e93d6ddd74d7dc088728b4a6bc9
Chayo Orozco <84361768+ChayoOrozco@users.noreply.github.com>
2024-03-09 23:30:19 -0600

    se especifico valifacion de log in y se agrego campo de codigo admin en registro

--- js/login.js
+++ js/login.js
@@ -1,22 +1,34 @@
 document.getElementById("btnRegister").addEventListener("click", function(event){
-    var correo = document.getElementById("correo").value;
-    var password = document.getElementById("password").value;
+    var correo = document.getElementById("correo").value.trim();
+    var password = document.getElementById("password").value.trim();
     var valid = true;
 
-    // Validar correo
-    if(!correo.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
-        document.getElementById("correoError").innerText = "El correo es incorrecto";
+    // Validar si el correo está vacío o solo tiene espacios
+    if(correo === ""){
+        document.getElementById("correoError").innerText = "Información obligatoria";
         valid = false;
     } else {
-        document.getElementById("correoError").innerText = "";
+        // Validar formato de correo
+        if(!correo.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+            document.getElementById("correoError").innerText = "El correo es incorrecto";
+            valid = false;
+        } else {
+            document.getElementById("correoError").innerText = "";
+        }
     }
 
-    // Validar contraseña
-    if(password.length < 6){
-        document.getElementById("passwordError").innerText =  "La contraseña es incorrecta.";
+    // Validar si la contraseña está vacía o solo tiene espacios
+    if(password === ""){
+        document.getElementById("passwordError").innerText = "Información obligatoria";
         valid = false;
     } else {
-        document.getElementById("passwordError").innerText = "";
+        // Validar longitud de contraseña
+        if(password.length < 6){
+            document.getElementById("passwordError").innerText = "La contraseña es incorrecta.";
+            valid = false;
+        } else {
+            document.getElementById("passwordError").innerText = "";
+        }
     }
 
     // Evitar el envío si hay errores
