ed5a7622179427df34d7e3ae2dbc26ea4d5a9d48
jost <jospmdz@gmail.com>
2024-03-12 22:14:53 -0600

    loguin y registro con js actualizado

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,15 @@
 function obtenerTipoUsuario() {
     // Obtener el tipo de usuario desde el almacenamiento local
-    // Si no existe, establecerlo en 0 (usuario normal) por defecto
-    var tipoUsuario = localStorage.getItem('tipoUsuario') || 0;
+    var tipoUsuario = localStorage.getItem('tipoUsuario');
     return tipoUsuario;
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     // Obtener el tipo de usuario y actualizar el navbar
     var tipoUsuario = obtenerTipoUsuario();
     actualizarNavbar(tipoUsuario);
 });
 
-// Función para actualizar el navbar según el tipo de usuario
 function actualizarNavbar(tipoUsuario) {
     // Obtener elementos del navbar
     var defaultDropdown = document.getElementById('defaultDropdown');
@@ -18,15 +17,17 @@ function actualizarNavbar(tipoUsuario) {
     var adminDropdown = document.getElementById("adminDropdown");
 
     // Si el tipo de usuario es administrador, mostrar el navbar de administrador y ocultar el de usuario normal
-    if (tipoUsuario == 1) {
+    if (tipoUsuario === "1") {
         userDropdown.classList.add('d-none');
         adminDropdown.classList.remove('d-none');
         defaultDropdown.classList.add('d-none');
-        
-    } else { // Si el tipo de usuario es usuario normal, mostrar el navbar de usuario normal y ocultar el de administrador
+    } else if (tipoUsuario === "0") { // Si el tipo de usuario es usuario normal
         userDropdown.classList.remove('d-none');
         adminDropdown.classList.add('d-none');
         defaultDropdown.classList.add('d-none');
-
+    } else { // Si no se ha definido un tipo de usuario o es otro valor
+        userDropdown.classList.add('d-none');
+        adminDropdown.classList.add('d-none');
+        defaultDropdown.classList.remove('d-none');
     }
 }
