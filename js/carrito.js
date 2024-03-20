14c624ef3bb89be45a54b12e1f9407aa8e6e802f
Zofinsky <sofiamv.design@gmail.com>
2024-03-19 21:19:01 -0600

    Se agregan cambios solicitados
    Se agregan cambios:
    1.Ocultar del logueo de usuario "iniciar sesión" y "Regístrate" (Excepto en ItemsViajes).
    2. Hacer más grande el logo del dropdown.
    3.Darle una ruta correcta al botón de "Ingrese" en la página de "Contacto" y cambiar el style del título.

---
+++ js/carrito.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    //Se obtiene miembro login de localStorage (OCULTAR DEL LOGUEO DE USUARIO)
+    let iniciar_sesionUN = document.getElementById("iniciar_sesionUN");
+    let Crear_cuentaUN = document.getElementById("Crear_cuentaUN");
+
+    let miembroLogin = localStorage.getItem("login");
+    if (miembroLogin != null){
+        iniciar_sesionUN.classList.add('d-none');
+        Crear_cuentaUN.classList.add('d-none');
+    } else {
+        iniciar_sesionUN.classList.remove('d-none');
+        Crear_cuentaUN.classList.remove('d-none');
+    }
+});
--- js/itemsViajes.js
+++ js/itemsViajes.js
@@ -101,6 +101,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const storedItem = viajes[i];
         addItem(storedItem);
     }
+
+            //Se obtiene miembro login de localStorage (OCULTAR DEL LOGUEO DE USUARIO)
+            let iniciar_sesionUN = document.getElementById("iniciar_sesionUN");
+            let Crear_cuentaUN = document.getElementById("Crear_cuentaUN");
+    
+            let miembroLogin = localStorage.getItem("login");
+            if (miembroLogin != null){
+                iniciar_sesionUN.classList.add('d-none');
+                Crear_cuentaUN.classList.add('d-none');
+            } else {
+                iniciar_sesionUN.classList.remove('d-none');
+                Crear_cuentaUN.classList.remove('d-none');
+            }
+
 });
 
 // Agrega un evento de clic al contenedor de las tarjetas para manejar clics en los botones
